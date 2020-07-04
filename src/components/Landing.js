d2f416da14296b65391ef3787f369bae18e0be27
Robert Lorch <lorchrob@gmail.com>
2020-07-03 19:47:36 -0700

    Add basic structure, include dependencies

---
+++ src/components/Landing.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Landing = () => {
+  return <div>Landing (Welcome to Runicorn!)</div>;
+};
+
+export default Landing;
---
+++ src/components/Login.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Login = () => {
+  return <div>Login</div>;
+};
+
+export default Login;
