c35e6c86bcd3464dfc798870094743458289b192
Frank Li <lifrank@grinnell.edu>
2020-07-05 02:23:00 -0500

    Revert "awfe"
    This reverts commit 8c6919bf91b27041ab970400209a00ba9d1f4c23.

--- src/server/handler.js
+++ src/server/handler.js
@@ -3,9 +3,7 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 
-import path from 'path';
-
-const html = readFileSync(path.join(__dirname, '../../build/index.html'));
+const html = readFileSync('../build/index.html');
 
 module.exports.render = (Tree, path) => {
   const markup = renderToString(
