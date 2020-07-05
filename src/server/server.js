89df94d882521228e6dd099f5d4e1580ae009f2e
Frank Li <lifrank@grinnell.edu>
2020-07-05 02:37:32 -0500

    Revert "add stuff"
    This reverts commit 6446811e8770cf4e1511cc059a415fa20c08e186.

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { render } from './handler';
-import App from '../client/App';
+import App from '../src/App';
 
 module.exports.app = express()
 .use('*', (req, res) => {
